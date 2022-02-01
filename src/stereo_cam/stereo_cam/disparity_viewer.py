525b2cae2fce7b035e4744eec2bced8bc7659292
frank20a <frank.fourlas@gmail.com>
2022-02-01 17:45:25 +0100

    Disparity map

--- src/stereo_cam/stereo_cam/disparity_viewer.py
+++ src/stereo_cam/stereo_cam/disparity_viewer.py
@@ -1,8 +1,10 @@
+from sympy import diag
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import CameraInfo, Image
+from sensor_msgs.msg import Image
 from rclpy.qos import QoSPresetProfiles
 from cv_bridge import CvBridge
+from stereo_msgs.msg import DisparityImage
 
 import cv2
 from time import time
@@ -11,33 +13,29 @@ import numpy as np
 bridge = CvBridge()
 stereo = cv2.StereoBM_create()
 
-def nothing(args):pass
+# Stereo parameters
+numDisparities = 16 * 4
+minDisparity = -10
 
-# Setting the updated parameters before computing disparity map
-cv2.namedWindow('disp',cv2.WINDOW_NORMAL)
-cv2.resizeWindow('disp',600,600)
-
-cv2.createTrackbar('numDisparities','disp',1,17,nothing)
-cv2.createTrackbar('blockSize','disp',5,50,nothing)
-cv2.createTrackbar('preFilterType','disp',1,1,nothing)
-cv2.createTrackbar('preFilterSize','disp',2,25,nothing)
-cv2.createTrackbar('preFilterCap','disp',5,62,nothing)
-cv2.createTrackbar('textureThreshold','disp',10,100,nothing)
-cv2.createTrackbar('uniquenessRatio','disp',15,100,nothing)
-cv2.createTrackbar('speckleRange','disp',0,100,nothing)
-cv2.createTrackbar('speckleWindowSize','disp',3,25,nothing)
-cv2.createTrackbar('disp12MaxDiff','disp',5,25,nothing)
-cv2.createTrackbar('minDisparity','disp',5,25,nothing)
-
+stereo.setNumDisparities(numDisparities)
+stereo.setBlockSize(25)
+stereo.setPreFilterSize(7)
+stereo.setPreFilterCap(20)
+stereo.setUniquenessRatio(15)
+stereo.setSpeckleRange(3)
+stereo.setMinDisparity(minDisparity)
 
 class ImageViewer(Node):
     def __init__(self):
         super().__init__("stereo_image_viewer")
-        
-        self.prev_t = time()
 
         self.img_r = None
+        self.updated_r = False
         self.img_l = None
+        self.updated_l = False
+
+        max_fps = 60
+        self.prev_t = time()
         
         self.create_subscription(
             Image,
@@ -52,47 +50,61 @@ class ImageViewer(Node):
             self.r_callback,
             QoSPresetProfiles.get_from_short_key('sensor_data')
         )
-
-        self.create_timer(1/30, self.disparity)
+        
+        self.publisher_disp = self.create_publisher(
+            DisparityImage, 
+            'stereo/disparity', 
+            QoSPresetProfiles.get_from_short_key('sensor_data')
+        )
+        
+        self.publisher_pcl = self.create_publisher(
+            PointCloud2, 
+            'stereo/pointcloud', 
+            QoSPresetProfiles.get_from_short_key('sensor_data')
+        )
+        
+        self.create_timer(1/max_fps, self.disparity)
 
     def l_callback(self, msg):
         self.img_l = cv2.cvtColor(bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough'), cv2.COLOR_BGR2GRAY)
+        self.updated_l = True
 
     def r_callback(self, msg):
         self.img_r = cv2.cvtColor(bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough'), cv2.COLOR_BGR2GRAY)
-    
+        self.updated_r = True
+
     def disparity(self):
-        numDisparities = cv2.getTrackbarPos('numDisparities','disp')*16
-        blockSize = cv2.getTrackbarPos('blockSize','disp')*2 + 5
-        preFilterType = cv2.getTrackbarPos('preFilterType','disp')
-        preFilterSize = cv2.getTrackbarPos('preFilterSize','disp')*2 + 5
-        preFilterCap = cv2.getTrackbarPos('preFilterCap','disp')
-        textureThreshold = cv2.getTrackbarPos('textureThreshold','disp')
-        uniquenessRatio = cv2.getTrackbarPos('uniquenessRatio','disp')
-        speckleRange = cv2.getTrackbarPos('speckleRange','disp')
-        speckleWindowSize = cv2.getTrackbarPos('speckleWindowSize','disp')*2
-        disp12MaxDiff = cv2.getTrackbarPos('disp12MaxDiff','disp')
-        minDisparity = cv2.getTrackbarPos('minDisparity','disp')
+        # ----> Check if both images are fresh
+        if not (self.updated_r and self.updated_l): return
+        self.updated_l = False
+        self.updated_r = False
 
-        stereo.setNumDisparities(numDisparities)
-        stereo.setBlockSize(blockSize)
-        stereo.setPreFilterType(preFilterType)
-        stereo.setPreFilterSize(preFilterSize)
-        stereo.setPreFilterCap(preFilterCap)
-        stereo.setTextureThreshold(textureThreshold)
-        stereo.setUniquenessRatio(uniquenessRatio)
-        stereo.setSpeckleRange(speckleRange)
-        stereo.setSpeckleWindowSize(speckleWindowSize)
-        stereo.setDisp12MaxDiff(disp12MaxDiff)
-        stereo.setMinDisparity(minDisparity)
 
-        if self.img_l is not None and self.img_r is not None:
-            # CAMERAS ARE OPPOSITE GAMW TO SYMPAN OLAKERO
-            disparity = stereo.compute(self.img_r, self.img_l).astype(np.float32)
-            disparity = (disparity/16.0 - minDisparity)/numDisparities
+        disparity = stereo.compute(self.img_l, self.img_r).astype(np.float32)
+        disparity = (disparity/16.0 - (minDisparity-1))/numDisparities
 
-            cv2.imshow("disp",disparity)
-            cv2.waitKey(1)
+        # ----> Send disparsity image message
+        # disp_msg = DisparityImage()
+        # disp_msg.max_disparity = 1.0
+        # disp_msg.min_disparity = 0.0
+        # disp_msg.delta_d = 1.0 / numDisparities
+        # disp_msg.image = bridge.cv2_to_imgmsg(disparity)
+        # disp_msg.t = 0.065
+        # disp_msg.f = (720 / 2) / np.tan(1.04699999 / 2)
+        # self.publisher_disp.publish(disp_msg)
+
+        # ----> Calculate FPS
+        fps = 1 / (time() - self.prev_t)
+        self.prev_t = time()
+        
+        # ----> Prepare disparsity image for preview
+        # print(np.max(disparity), np.min(disparity))
+        cv2.putText(disparity, "FPS: {0:d}".format(int(np.ceil(fps))), (80, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 0), 3)
+        
+
+        # ----> Show disparsity image
+        cv2.imshow("Disparity Map",disparity)
+        cv2.waitKey(1)
 
 
 def main(args=None):
