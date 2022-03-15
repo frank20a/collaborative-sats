cc4439c1af3700612178583113c9fe4754385791
frank20a <frank.fourlas@gmail.com>
2022-03-15 22:08:40 +0100

    Aruco Cube simulation

--- src/pose_estimation/launch/sim.launch.py
+++ src/pose_estimation/launch/sim.launch.py
@@ -7,51 +7,85 @@ import xacro, os
 
 def generate_launch_description():
 
-    raw_robot = xacro.process_file('/home/frank20a/dev-ws/models/simple_targets/marker_cube.urdf.xacro').toxml()
+    raw_target = xacro.process_file('/home/frank20a/dev-ws/models/simple_targets/marker_cube.urdf.xacro').toxml()
+    raw_chaser = xacro.process_file('/home/frank20a/dev-ws/models/chaser/chaser.urdf.xacro').toxml()
 
+    world = os.path.join('/home/frank20a/dev-ws', 'worlds', 'space.world')
     
-    return LaunchDescription([
+
+    return LaunchDescription([ 
+    
         # Open Gazebo
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([os.path.join(get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
+            launch_arguments={'world': world}.items()
         ),
 
-        # Publish robot to robot_state_publisher
+        # Publish target to robot_state_publisher
         Node(
             package = 'robot_state_publisher',
             executable =  'robot_state_publisher',
             name = 'state_publisher',
             output = 'screen',
             parameters = [{
-                'robot_description': raw_robot,
+                'robot_description': raw_target,
                 'use_sim_time': True    # Use simulation ()
                 }],
             remappings=[
-                ('robot_description', 'easy_cube_desc')
+                ('robot_description', 'target_cube_desc')
+            ]
+        ), 
+        
+        # Publish chaser to robot_state_publisher
+        Node(
+            package = 'robot_state_publisher',
+            executable =  'robot_state_publisher',
+            name = 'state_publisher',
+            output = 'screen',
+            parameters = [{
+                'robot_description': raw_chaser,
+                'use_sim_time': True    # Use simulation ()
+                }],
+            remappings=[
+                ('robot_description', 'chaser_desc')
             ]
         ),
 
-        # # Open RViz
-        # Node(
-        #     package='rviz2',
-        #     executable='rviz2',
-        #     parameters = [{
-        #         'use_sim_time': True
-        #     }],
-        #     # arguments=[
-        #     #     '-d', os.path.join(get_package_share_directory('stereo_cam'), 'rviz_config/stereo_cam.rviz')
-        #     # ]
-        # ),
+        # Open RViz
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            parameters = [{
+                'use_sim_time': True
+            }],
+            arguments=[
+                '-d', os.path.join(get_package_share_directory('pose_estimation'), 'rviz_config/space_sim.rviz')
+            ]
+        ),
 
-        # Spawn robot into Gazebo
+        # Spawn target into Gazebo
         Node(
             package='gazebo_ros',
             executable='spawn_entity.py',
             name='spawner',
             arguments=[
-                '-topic', 'easy_cube_desc', 
-                '-entity', 'marker_cube'
-                ]
+                '-topic', '/target_cube_desc', 
+                '-entity', 'marker_cube',
+                '-x', '1',
+                '-y', '0',
+                '-z', '1'
+            ]
+        ),
+        
+        # Spawn chaser into Gazebo
+        Node(
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            name='spawner',
+            arguments=[
+                '-topic', '/chaser_desc', 
+                '-entity', 'chaser'
+            ]
         ),
 
         # Spawn a disparity publisher
