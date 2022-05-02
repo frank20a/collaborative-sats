Work on MPC w/ Quaternions + Quaternion model fixed + Thrust rotation implemented in model - Torque rotation implemented in controller NOT model

src/control/control/mpc_controller.py
@@ -28,15 +28,22 @@ class MPCController(Node):
         
         self.solver = chaser_mpc.solver()            
         
-        self.setpoint = Pose()
-        self.setpoint.position.x = -1.0
-        self.setpoint.position.y = -1.0
-        self.setpoint.position.z = 1.0
-        q = quaternion_from_euler(0.0, 0.0, 0.0)
-        self.setpoint.orientation.x = q[0]
-        self.setpoint.orientation.y = q[1]
-        self.setpoint.orientation.z = q[2]
-        self.setpoint.orientation.w = q[3]
+        q = quaternion_from_euler(0.0, 0.0, np.pi)
+        self.setpoint = np.array([
+            -1,         # x
+            -1,         # y
+            1,          # z
+            0,          # x_dot
+            0,          # y_dot
+            0,          # z_dot 
+            q[0],       # qx
+            q[1],       # qy
+            q[2],       # qz
+            q[3],       # qw
+            0,          # wx
+            0,          # wy
+            0           # wz
+        ], dtype=np.float64)
 
         self.create_subscription(
             Pose,
@@ -51,10 +58,7 @@ class MPCController(Node):
             QoSPresetProfiles.get_from_short_key('sensor_data')
         )
         
-        
-        # self.publisher = self.create_publisher(Int16, 'thrust_cmd', QoSPresetProfiles.get_from_short_key('system_default'))
         self.publisher = self.create_publisher(Wrench, 'thrust_cmd', QoSPresetProfiles.get_from_short_key('system_default'))
-        
         
         if self.verbose > 1:
             self.debug_setpoint_xyz = self.create_publisher(Vector3, 'debug/setpoint_xyz', QoSPresetProfiles.get_from_short_key('sensor_data'))
@@ -65,10 +69,10 @@ class MPCController(Node):
     def callback(self, msg: Odometry):
                 
         # Get state for optimizer
-        state = get_state(self.setpoint, msg)
+        state = get_state(msg)
         
         # Call the optimizer
-        resp = self.solver.run(p=state)
+        resp = self.solver.run(p=np.concatenate((state, self.setpoint)))
         # self.get_logger().info('Response: {}'.format(resp.solution[:6]))
         try:
             u = np.array(resp.solution)[:6]
@@ -79,16 +83,8 @@ class MPCController(Node):
         # return
         
         # Rotate the thrust vector to remove body frame orientation
-        u[0:3] = vector_rotate_quaternion(u[0:3], quaternion_inverse(odometry2array(msg)[3:]))
-        
-        # cmd = Int16()
-        # cmd.data = 0
-        # for i, f in enumerate(u):
-        #     # Skip roll and pitch control
-        #     if i == 3 or i == 4: continue
-            
-        #     if f != 0:
-        #         cmd.data |= flags[2 * i + (1 if f < 0 else 0)]
+        # u[0:3] = vector_rotate_quaternion(u[0:3], quaternion_inverse(odometry2array(msg)[3:]))
+        u[3:6] = vector_rotate_quaternion(u[3:6], quaternion_inverse(odometry2array(msg)[3:]))
         
         cmd = Wrench()
         cmd.force.x = u[0] / force
@@ -104,13 +100,13 @@ class MPCController(Node):
         if self.verbose > 1:
             
             tmp = Vector3()
-            tmp.x = self.setpoint.position.x
-            tmp.y = self.setpoint.position.y
-            tmp.z = self.setpoint.position.z
+            tmp.x = self.setpoint[0]
+            tmp.y = self.setpoint[1]
+            tmp.z = self.setpoint[2]
             self.debug_setpoint_xyz.publish(tmp)
             
             tmp = Vector3()
-            euler = euler_from_quaternion([self.setpoint.orientation.x, self.setpoint.orientation.y, self.setpoint.orientation.z, self.setpoint.orientation.w])
+            euler = euler_from_quaternion(self.setpoint[6:10])
             tmp.x = euler[0] * 180 / np.pi
             tmp.y = euler[1] * 180 / np.pi
             tmp.z = euler[2] * 180 / np.pi

src/control/control/tf_utils/__init__.py
@@ -113,21 +113,19 @@ def pose2array_euler(pose: Pose) -> np.ndarray:
 
 
 # Temporary until difference includeds Twist too... for now zero target twist is assumed
-def get_state(target: Pose, odom: Odometry) -> np.ndarray:
-    
-    diff = get_pose_diff(target, odometry2pose(odom))
+def get_state(odom: Odometry) -> np.ndarray:
 
     return np.array([
-        diff.position.x,
-        diff.position.y,
-        diff.position.z,
+        odom.pose.pose.position.x,
+        odom.pose.pose.position.y,
+        odom.pose.pose.position.z,
         odom.twist.twist.linear.x,
         odom.twist.twist.linear.y,
         odom.twist.twist.linear.z,
-        diff.orientation.x,
-        diff.orientation.y,
-        diff.orientation.z,
-        diff.orientation.w,
+        odom.pose.pose.orientation.x,
+        odom.pose.pose.orientation.y,
+        odom.pose.pose.orientation.z,
+        odom.pose.pose.orientation.w,
         odom.twist.twist.angular.x,
         odom.twist.twist.angular.y,
         odom.twist.twist.angular.z,