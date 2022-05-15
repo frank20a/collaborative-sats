ce5e57aa5c0af3063ae85e5de3dd90f5f8de1bce
Frank Fourlas <frank.fourlas@gmail.com>
2022-05-15 02:16:05 +0200

    Combine Estimations performs derivation

--- src/pose_estimation/pose_estimation/combine_estimations.py
+++ src/pose_estimation/pose_estimation/combine_estimations.py
@@ -1,14 +1,21 @@
 import rclpy
 from rclpy.time import Time
 from rclpy.node import Node
-from geometry_msgs.msg import TransformStamped
+from geometry_msgs.msg import TransformStamped, Pose, Twist
+from std_msgs.msg import Empty
 from tf2_ros import TransformBroadcaster, TransformException
 from tf2_ros import LookupException, ConnectivityException, ExtrapolationException
 from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
 from tf_transformations import euler_from_quaternion, quaternion_from_euler
+from rclpy.qos import QoSPresetProfiles
 
 import numpy as np
+
+
+abs = lambda x: x if x > 0 else -x
+floor_ = lambda x, n=1e-2: x if abs(x) > n else 0.0
+sgn = lambda x: (1 if x > 0 else -1) if floor_(x, 1e-2) != 0 else 0
 
 
 class CombineEstimations(Node):
@@ -19,19 +26,37 @@ class CombineEstimations(Node):
         self.declare_parameter('num_chasers', 2)
         self.declare_parameter('freq', 30.0)
         self.declare_parameter('verbose', 0)
+        self.declare_parameter('topics', True)
+        self.declare_parameter('ra_len', 9)
         
         self.num_chasers = self.get_parameter('num_chasers').get_parameter_value().integer_value
-        self.freq = self.get_parameter('freq').get_parameter_value().double_value
+        self.dt = 1 / self.get_parameter('freq').get_parameter_value().double_value
         self.verbose = self.get_parameter('verbose').get_parameter_value().integer_value
+        self.topics = self.get_parameter('topics').get_parameter_value().bool_value        
+        self.ra_len = self.get_parameter('ra_len').get_parameter_value().integer_value
+
         
         # create a tf2 buffer and listener
         self.buffer = Buffer()
         self.listener = TransformListener(self.buffer, self)
+
+        if self.topics:
+            self.p_pub = self.create_publisher(Pose, 'estimated_pose', QoSPresetProfiles.get_from_short_key('sensor_data'))
+            self.t_pub = self.create_publisher(Twist, 'estimated_twist', QoSPresetProfiles.get_from_short_key('sensor_data'))
+            self.create_subscription(Empty, 'toggle_avg_twist', self.avg_callback, QoSPresetProfiles.get_from_short_key('sensor_data'))
+
+            self.prev_vels = None
+            self.prev_state = None
+            self.avg = False
         
         self.tfs = [None for i in range(self.num_chasers)]
 
-        self.create_timer(1 / self.freq, self.combine_n_publish)
-        
+        self.create_timer(self.dt, self.combine_n_publish)
+
+    def avg_callback(self, msg):
+        self.avg = not self.avg
+        if self.verbose > 0: self.get_logger().info('Toggled avg to {}'.format(self.avg))
+
     def combine_n_publish(self):
         # create a combined transform
         combined_transform = TransformStamped()
@@ -86,6 +111,82 @@ class CombineEstimations(Node):
         # publish the combined transform
         if self.verbose > 0: self.get_logger().info('Publishing combined transform')
         self.broadcaster.sendTransform(combined_transform)
+
+        if self.topics:
+            self.publish_state(combined_transform)
+
+    def publish_state(self, combined_transform):
+        p = Pose()
+        p.position.x = combined_transform.transform.translation.x
+        p.position.y = combined_transform.transform.translation.y
+        p.position.z = combined_transform.transform.translation.z
+        p.orientation.x = combined_transform.transform.rotation.x
+        p.orientation.y = combined_transform.transform.rotation.y
+        p.orientation.z = combined_transform.transform.rotation.z
+        p.orientation.w = combined_transform.transform.rotation.w
+        self.p_pub.publish(p)
+
+        t = Twist()
+        eul = np.array(euler_from_quaternion([
+            combined_transform.transform.rotation.x,
+            combined_transform.transform.rotation.y, 
+            combined_transform.transform.rotation.z, 
+            combined_transform.transform.rotation.w])
+        )
+        if self.prev_state is None or self.prev_vels is None: 
+            v = np.zeros(3)
+            omega = np.zeros(3)
+            self.prev_vels = np.zeros((1, 6))
+        else:
+            # Calculate velocity
+            v = [
+                (combined_transform.transform.translation.x - self.prev_state[0]) / self.dt,
+                (combined_transform.transform.translation.y - self.prev_state[1]) / self.dt,
+                (combined_transform.transform.translation.z - self.prev_state[2]) / self.dt,
+            ]                   
+
+            # Calculate omega
+            try:
+                eul_diff = eul - self.prev_state[3:6]
+            except IndexError:
+                self.get_logger().info("{}".format(self.prev_state))
+                raise Exception()
+            omega = eul_diff / self.dt
+
+            # Post-process velocity
+            for i in range(3):
+                v[i] = v[i] if abs(v[i] - self.prev_vels[-1][i]) < 10 else self.prev_vels[-1][i]
+                v[i] = floor_(v[i], 0.1)
+                omega[i] = omega[i] if abs(omega[i] - self.prev_vels[-1][i + 3]) < 2 else self.prev_vels[-1][i + 3]
+                omega[i] = floor_(omega[i], 0.035)
+        
+        self.prev_state = np.array([
+            p.position.x,
+            p.position.y,
+            p.position.z,
+            eul[0],
+            eul[1],
+            eul[2]
+        ])
+        self.prev_vels = np.concatenate((self.prev_vels, np.array([[
+            v[0],
+            v[1],
+            v[2],
+            omega[0],
+            omega[1],
+            omega[2]
+        ]], dtype=np.float32)))
+        self.prev_vels = self.prev_vels[-150:]
+
+        ra_len = min(len(self.prev_vels), self.ra_len)
+        t.linear.x = floor_(np.average(self.prev_vels[-ra_len:, 0]))
+        t.linear.y = floor_(np.average(self.prev_vels[-ra_len:, 1]))
+        t.linear.z = floor_(np.average(self.prev_vels[-ra_len:, 2]))
+        t.angular.x = floor_(np.average(self.prev_vels[-ra_len:, 3]))
+        t.angular.y = floor_(np.average(self.prev_vels[-ra_len:, 4]))
+        t.angular.z = floor_(np.average(self.prev_vels[-ra_len:, 5]))
+
+        self.t_pub.publish(t)
 
 
 def main(args=None):
